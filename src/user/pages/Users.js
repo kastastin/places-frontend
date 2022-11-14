e050882c115ade6b4c0357da838b32acadb4c6ba
kastastin <kostya242421@gmail.com>
2022-11-14 12:10:45 +0200

    UsersList + UserItem

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,7 +1,18 @@
 import React from 'react';
 
+import UsersList from '../components/UsersList';
+
 const Users = () => {
-  return <h2>Users Works!</h2>;
+  const USERS = [
+    {
+      id: 'u1',
+      name: 'kastastin',
+      image: 'https://images.unsplash.com/photo-1602992708529-c9fdb12905c9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fGRldmVsb3BlcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60',
+      places: 3
+    }
+  ];
+
+  return <UsersList items={USERS} />
 };
 
 export default Users;
